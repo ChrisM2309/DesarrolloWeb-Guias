d5ad2da81494d852f734e9bb67e56c6416c01857
ChrisM2309 <chris.marroquin2309@gmail.com>
2025-11-03 13:51:23 -0600

    Guia 6 - Ejercicio complementario 1 - Terminado

--- 20245332_Guia6/js/formulario.js
+++ 20245332_Guia6/js/formulario.js
@@ -22,9 +22,22 @@ const mensaje = document.getElementById('idMensaje');
 
 const idModal = document.getElementById('idModal');
 
+// MODAL PARA EDITAR VALORES 
+const idModalEditar = document.getElementById('idModalEditar');
+const inputNombreEditar = document.getElementById('idTxtNombreEditar');
+const inputApellidoEditar = document.getElementById('idTxtApellidoEditar');
+const inputFechaNacimientoEditar = document.getElementById('idTxtFechaNacimientoEditar');
+const inputRdMasculinoEditar = document.getElementById('idRdMasculinoEditar');
+const inputRdFemeninoEditar = document.getElementById('idRdFemeninoEditar');
+const cmbPaisEditar = document.getElementById('idCmbPaisEditar');
+const inputDireccionEditar = document.getElementById('idTxtDireccionEditar');
+const buttonGuardarCambiosEditar = document.getElementById('idBtnGuardarCambios');
+
 // Arreglo global de todos los pacientes 
 
 let arrayPaciente = [];
+//arrayPaciente.push(new Array("nombre", "apellido", "11223344", "Masculino", "El Salvador", "direccion"));
+//arrayPaciente.push(new Array("nombre2", "apellido2", "11223344", "Masculino", "El Salvador", "direccion"));
 
 // Funciones
 
@@ -74,7 +87,6 @@ const addPaciente = function () {
 
 }
 // Imprimir todos los pacientes registrados 
-
 // Mostrar las filas 
 
 function imprimirFilas() {
@@ -91,7 +103,7 @@ function imprimirFilas() {
         <td> ${element[4]} </td>
         <td> ${element[5]} </td>
         <td> 
-            <button id = "BtnEditar${contador}" type = "button" class = "btn btn-primary" alt = "Eliminar"> 
+            <button id = "BtnEditar${contador}" type = "button" class = "btn btn-primary" alt = "Eliminar" data-bs-toggle="modal" data-bs-target="#idModalEditar"> 
                 <i class = "bi bi-pencil-square"> </i> 
             </button> 
             <button id = "BtnEliminar${contador}" type = "button" class = "btn btn-danger" alt = "Editar"> 
@@ -102,9 +114,99 @@ function imprimirFilas() {
         `;
         contador++;
     });
-
     return $fila;
 };
+
+function agregarBotonesEditar() {
+    // Por cada elemento agregar
+    let cantidadPacientes = arrayPaciente.length;
+    for (let i = 1; i <= cantidadPacientes; i++) {
+        const idActual = `BtnEditar${i}`;
+        const pacienteActual = arrayPaciente[i - 1];
+        const botonActual = document.getElementById(idActual);
+
+        if (botonActual) {
+            botonActual.addEventListener("click", function () {
+                //? Al hacer click, cargar datos actuales en los campos correspondientes 
+                // Los datos estan guardados en formato  (nombre, apellido, fechaNacimiento, sexo, labelPais, direccion));
+                inputNombreEditar.value = pacienteActual[0];
+                inputApellidoEditar.value = pacienteActual[1];
+                inputFechaNacimientoEditar.value = pacienteActual[2];
+                if (pacienteActual[3] == 'Masculino') {
+                    inputRdMasculinoEditar.checked = true;
+                    inputRdFemeninoEditar.checked = false;
+                } else {
+                    inputRdFemeninoEditar.checked = true;
+                    inputRdMasculinoEditar.checked = false;
+                }
+                cmbPaisEditar.value = Array.from(cmbPaisEditar.options).find(option => option.text.trim() == pacienteActual[4].trim()).value;
+
+                console.log(pacienteActual[5]);
+                inputDireccionEditar.value = pacienteActual[5];
+
+                //! Hacer que cuando se de a boton guardar, se actualicen los datos de este campo
+
+                buttonGuardarCambiosEditar.onclick = function () {
+                    let nombre = inputNombreEditar.value;
+                    let apellido = inputApellidoEditar.value;
+                    let fechaNacimiento = inputFechaNacimientoEditar.value;
+                    let sexo = inputRdMasculinoEditar.checked == true ? 'Masculino' : inputRdFemeninoEditar.checked == true ? 'Femenino' : '';
+                    let pais = cmbPaisEditar.value;
+                    let labelPais = cmbPaisEditar.options[cmbPaisEditar.selectedIndex].text;
+                    let direccion = inputDireccionEditar.value;
+
+                    if (nombre != '' &&
+                        apellido != '' &&
+                        fechaNacimiento != '' &&
+                        sexo != '' &&
+                        pais != 0 &&
+                        direccion != '') {
+                        // Cambiar el paciente actual
+                        // Los datos estan guardados en formato  (nombre, apellido, fechaNacimiento, sexo, labelPais, direccion));
+                        arrayPaciente[i-1][0] = nombre;
+                        arrayPaciente[i-1][1] = apellido; 
+                        arrayPaciente[i-1][2] = fechaNacimiento; 
+                        arrayPaciente[i-1][3] = sexo;
+                        arrayPaciente[i-1][4] = labelPais;
+                        arrayPaciente[i-1][5] = direccion; 
+                        //arrayPaciente.push(new Array(nombre, apellido, fechaNacimiento, sexo, labelPais, direccion));
+                        mensaje.innerHTML = "Se ha editado al paciente.";
+                        toast.show();
+                        imprimirPacientes();
+                        //limpiarForm();
+                    } else {
+                        mensaje.innerHTML = "No se ha editado al paciente. Faltan datos por completar.";
+                        toast.show();
+                    }
+                }
+            });
+        } else {
+            console.warn(`No se encontró el botón con id ${idActual}`);
+        }
+    }
+
+}
+
+function agregarBotonesEliminar() {
+    // Por cada elemento agregar
+    let cantidadPacientes = arrayPaciente.length;
+    for (let i = 1; i <= cantidadPacientes; i++) {
+        const idActual = `BtnEliminar${i}`;
+        const botonActual = document.getElementById(idActual);
+
+        if (botonActual) {
+            botonActual.onclick = function () {
+                console.log(`Click en botón Eliminar ${i}`);
+                arrayPaciente.splice(i-1,1);
+                imprimirPacientes();
+            };
+        } else {
+            console.warn(`No se encontró el botón con id ${idActual}`);
+        }
+    }
+
+}
+
 
 // Mostrar pacientes 
 const imprimirPacientes = () => {
@@ -124,6 +226,9 @@ const imprimirPacientes = () => {
         </table>
     </div>`;
     document.getElementById("idTablaPacientes").innerHTML = $table;
+    // Agregar configuracion de botones 
+    agregarBotonesEditar();
+    agregarBotonesEliminar();
 };
 
 // Opcion de paises, contador global
@@ -134,17 +239,19 @@ const addPais = () => {
     let paisNew = inputNombrePais.value;
 
     if (paisNew != '') {
-        
+
         // Agregar pais
 
         let option = document.createElement("option");
         option.textContent = paisNew;
 
         cmbPais.appendChild(option);
+        const option2 = option1.cloneNode(true); 
+        cmbPaisEditar.appendChild(option2);
         mensaje.innerHTML = "Pais agregado correctamente";
         toast.show();
 
-    } else { 
+    } else {
         mensaje.innerHTML = "Faltan campos por completar";
         toast.show();
     }
@@ -172,5 +279,6 @@ idModal.addEventListener("shown.bs.modal", () => {
     inputNombrePais.value = "";
     inputNombrePais.focus();
 })
+
 
 limpiarForm();
